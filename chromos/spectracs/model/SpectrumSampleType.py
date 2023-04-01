39d2d03962687045d85d86575c8feb4ab3c6f98e
nidwe72 <edwin.roth@gmx.at>
2023-04-01 22:53:56 +0200

    * spectracsPy-model: added SpectrumSampleType.py

---
+++ chromos/spectracs/model/SpectrumSampleType.py
@@ -0,0 +1,10 @@
+
+from enum import Enum
+
+class SpectrumSampleType(Enum):
+    REFERENCE='REFERENCE'
+    SAMPLE='SAMPLE'
+    DARK='DARK'
+    BLANK='BLANK'
+    UNSPECIFIED = 'UNSPECIFIED'
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='spectracsPy-model',
+    version='1.0.0',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='nidwe',
+    author_email='',
+    description=''
+)
