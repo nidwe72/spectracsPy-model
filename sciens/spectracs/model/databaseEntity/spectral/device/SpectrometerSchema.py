09ee3fa2103e925b993f2867efcb47ff15489fab
nidwe72 <edwin.roth@gmx.at>
2023-08-20 12:31:13 +0200

    * [SpectracsPyServer] * updated serialization schemas

--- sciens/spectracs/model/databaseEntity/spectral/device/SpectrometerSchema.py
+++ sciens/spectracs/model/databaseEntity/spectral/device/SpectrometerSchema.py
@@ -1,10 +1,19 @@
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow_sqlalchemy.fields import Nested
 
 from sciens.spectracs.model.databaseEntity.spectral.device.Spectrometer import Spectrometer
+from sciens.spectracs.model.databaseEntity.spectral.device.SpectrometerSensorSchema import SpectrometerSensorSchema
+from sciens.spectracs.model.databaseEntity.spectral.device.SpectrometerStyleSchema import SpectrometerStyleSchema
+from sciens.spectracs.model.databaseEntity.spectral.device.SpectrometerVendorSchema import SpectrometerVendorSchema
 
 
 class SpectrometerSchema(SQLAlchemyAutoSchema):
+
     class Meta:
         model = Spectrometer
         include_relationships = True
         load_instance = True
+
+    spectrometerSensor = Nested(SpectrometerSensorSchema, many=False)
+    spectrometerVendor = Nested(SpectrometerVendorSchema, many=False)
+    spectrometerStyle = Nested(SpectrometerStyleSchema, many=False)
--- sciens/spectracs/model/databaseEntity/spectral/device/SpectrometerSensorSchema.py
+++ sciens/spectracs/model/databaseEntity/spectral/device/SpectrometerSensorSchema.py
@@ -1,9 +1,16 @@
+from marshmallow.fields import Nested
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
 from sciens.spectracs.model.databaseEntity.spectral.device.SpectrometerSensor import SpectrometerSensor
+from sciens.spectracs.model.databaseEntity.spectral.device.SpectrometerSensorChip import SpectrometerSensorChip
+from sciens.spectracs.model.databaseEntity.spectral.device.SpectrometerSensorChipSchema import \
+    SpectrometerSensorChipSchema
 
 
 class SpectrometerSensorSchema(SQLAlchemyAutoSchema):
+
+    spectrometerSensorChip = Nested(SpectrometerSensorChipSchema, many=False)
+
     class Meta:
         model = SpectrometerSensor
         include_relationships = True
